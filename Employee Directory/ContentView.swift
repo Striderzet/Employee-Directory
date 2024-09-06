2fdcb8aa1e981febafd083448ff8409c5e767859
Tony Buckner <tony.buckner26@gmail.com>
2024-09-06 09:29:51 -0400

    Initial Commit

---
+++ Employee Directory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Employee Directory
+//
+//  Created by Tony Buckner on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Employee Directory/Employee_DirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Employee_DirectoryApp.swift
+//  Employee Directory
+//
+//  Created by Tony Buckner on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Employee_DirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
